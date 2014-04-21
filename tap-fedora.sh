1c02bd0f071e2089c239c24fee7ca15f682d720f
lofyer <lofyer@gmail.com>
2014-04-21 21:26:25 +0800

    Migrate to github.com

--- tap-fedora.sh
+++ tap-fedora.sh
@@ -2,13 +2,18 @@
 ulimit -l unlimited
 modprobe kvm
 modprobe kvm-intel
+#modprobe kvm-amd
 modprobe vfio
 modprobe vfio-pci
-modprobe vfio_iommu_type1
+#modprobe vfio_iommu_type1
 
 tunctl -t tap0
 tunctl -t tap1
 tunctl -t tap2
+
+ifconfig tap0 up
+ifconfig tap1 up
+ifconfig tap2 up
 
 brctl addif br0 tap0
 brctl addif br0 tap1
