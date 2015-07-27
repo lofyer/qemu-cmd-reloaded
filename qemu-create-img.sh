5ae5a97be9b9e4d523755d674c73e1dd8471e741
lofyer <lofyer@gmail.com>
2015-07-27 15:03:52 +0800

    DO NOT add -f

--- qemu-create-img.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-qemu-img create -f raw new-hda.raw 20G
-
-#qemu-img create -f qcow2 -o preallocation=metadata centos_test0_preallc.qcow2 20G
-
-#qemu-img convert -f qcow2 -O qcow2 -o preallocation=metadata centos_test0_nopreallc.qcow2        centos_test0_preallc.qcow2
